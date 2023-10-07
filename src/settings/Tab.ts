9c90b3f5ab51cb4a6d2c8df30d48c99c875eb177
James Reid <jamesreid3806@gmail.com>
2023-10-07 16:31:59 +0100

    doc= Add documentation for symlink tab class

--- src/settings/Tab.ts
+++ src/settings/Tab.ts
@@ -30,15 +30,20 @@ import type { Symlink } from "../main.ts"
 
 // @@body
 /**
- *
+ * Obsidian symlink settings tab class for rendering required markup in the
+ * settings tab, which allows the user to view and update current plugin
+ * settings. Note that each setting is controlled using an instance of a given
+ * controller class, all of which are instantiated and mounted by this class.
  */
 class SymlinkSettingsTab extends PluginSettingTab {
     plugin: Symlink
 
     /**
+     * Call obsidian plugin tab super, and set class property reference to the
+     * current instance of the symlink plugin.
      *
-     * @param app
-     * @param plugin
+     * @param app - Instance of obsidian app passed to settings tab manager.
+     * @param plugin - Instance of plugin passed to settings tab manager.
      */
     constructor(app: App, plugin: Symlink) {
         super(app, plugin)
@@ -46,16 +51,29 @@ class SymlinkSettingsTab extends PluginSettingTab {
     }
 
     /**
-     *
+     * Render required symlink settings controllers to settings tab container.
      */
     display(): void {
+        // Fetch container for settings tab, and empty it.
         const { containerEl } = this
         containerEl.empty()
 
+        // Fetch all repos, and map over them, creating an array of obsidian
+        // option objects which can be used in dropdown setting menus.
         const repos = this.plugin.getRepos().map(pathname => {
             return { value: pathname, display: pathname }
         })
 
+        // Create and mount each symlink settings controller required (one
+        // controller for each setting available in the symlink plugin).
+
+        // Note that excess string whitespace is ignored when rendered to the
+        // DOM. This means that although the multiline descriptions have
+        // incorrect tab spacing on new lines (i.e. extra whitespace), they
+        // appear correctly (i.e. without extra whitespace) when rendered in the
+        // obsidian settings tab.
+
+        // Controller for `repositoryDirIgnore` setting.
         new SymlinkSettingListController({
             title: "",
             description: "",
@@ -70,6 +88,7 @@ class SymlinkSettingsTab extends PluginSettingTab {
             setting: "repositoryDirIgnore"
         }).mount()
 
+        // Controller for `repositoryDirLink` setting.
         new SymlinkSettingListController({
             title: "",
             description: "",
@@ -84,6 +103,7 @@ class SymlinkSettingsTab extends PluginSettingTab {
             setting: "repositoryDirLink"
         }).mount()
 
+        // Controller for `repositoryIgnore` setting.
         new SymlinkSettingDropdownController({
             title: "",
             description: "",
@@ -100,6 +120,7 @@ class SymlinkSettingsTab extends PluginSettingTab {
             options: repos
         }).mount()
 
+        // Controller for `repositoryInclude` setting.
         new SymlinkSettingDropdownController({
             title: "",
             description: "",
@@ -116,6 +137,7 @@ class SymlinkSettingsTab extends PluginSettingTab {
             options: repos
         }).mount()
 
+        // Controller for `isWhitelist` setting.
         new SymlinkSettingToggleController({
             title: "",
             description: "",
@@ -129,6 +151,7 @@ class SymlinkSettingsTab extends PluginSettingTab {
             setting: "isWhitelist"
         }).mount()
 
+        // Controller for `shouldSymlinkOnStart` setting.
         new SymlinkSettingToggleController({
             title: "",
             description: "",
